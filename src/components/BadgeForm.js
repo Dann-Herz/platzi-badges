7b8166b275152110c7c6c3adb8b22cc6b4923020
Dann Herz <danielhdzmzin@gmail.com>
2021-06-22 10:47:28 -0500

    Was add the set.state and news inputs to obtein more info from the user

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+    state = {
+        //jobTitle: Designer
+    };
     handleChange = e => {
-        //console.log({ value = e.target.value });
-        console.log({ 
-            name: e.target.name,
-            value: e.target.value,
+        // //console.log({ value = e.target.value });
+        // console.log({ 
+        //     name: e.target.name,
+        //     value: e.target.value,
             
-        });
+        // });
+        this.setState({
+            // firstName: e.target.value
+            [e.target.name]: e.target.value,
+        })
     };
 
     handleClick = e => {
@@ -17,6 +24,8 @@ class BadgeForm extends React.Component {
 
     handleSubmit = e => {
         e.preventDefault();
+        console.log('Form was submited');
+        console.log(this.state);
     };
 
 
@@ -26,9 +35,55 @@ class BadgeForm extends React.Component {
                 <h1>New Attendant</h1>
 
                 <form onSubmit={this.handleSubmit}>
-                    <div className="">
+                    <div className="form-group">
                         <label>First Name</label>
-                        <input onChange={this.handleChange}  className="form-control" type="text" name="firstname"></input>
+                        <input 
+                        onChange={this.handleChange}  
+                        className="form-control" 
+                        type="text" 
+                        name="firstname"
+                        value={this.state.firstname}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input 
+                        onChange={this.handleChange}  
+                        className="form-control" 
+                        type="text" 
+                        name="lastName"
+                        value={this.state.lastName}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input 
+                        onChange={this.handleChange}  
+                        className="form-control" 
+                        type="email" 
+                        name="email"
+                        value={this.state.email}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Job Title</label>
+                        <input 
+                        onChange={this.handleChange}  
+                        className="form-control" 
+                        type="text" 
+                        name="jobTitle"
+                        value={this.state.jobTitle}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Twitter</label>
+                        <input 
+                        onChange={this.handleChange}  
+                        className="form-control" 
+                        type="text" 
+                        name="twitter"
+                        value={this.state.twitter}
+                        ></input>
                     </div>
                     <br></br>
                     <button type="submit" onClick={this.handleClick} className="btn btn-primary">Save</button>
