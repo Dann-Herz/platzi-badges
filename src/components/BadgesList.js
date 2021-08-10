c47dcbd7274e193c95115f2b46b89194de443087
Dann Herz <danielhdzmzin@gmail.com>
2021-08-10 09:40:52 -0500

    I finish the containers with names and img to show who will come to the conference

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import '../pages/styles/BadgesList.css'
+import twitterIcon from '../images/twitter-icon.png'
 
 class BadgesList extends React.Component {
     render() {
         return (
-            <ul className="list-unstyled">
+            <ul className="container">
             {this.props.badges.map((badge) => {
                 return (
-                    <li key={badge.id}>
-                        <p>
-                            {badge.firstName} {badge.lastName}
-                        </p>
+                    <li className="container__list-styled" key={badge.id}>
+                        <img className="container__list-styled__img" src={badge.avatarUrl} alt="" />
+                        <li>
+                            <div className="bold">
+                                {badge.firstName} {badge.lastName}
+                            </div>
+                            <div className="container__list-styled__twitter">
+                                <img src={twitterIcon} className="container__list-styled__twitter__img" alt="" />
+                                @{badge.twitter} 
+                                
+                            </div>
+                            <div>
+                                {badge.jobTitle} 
+                            </div>
+                        </li>
                     </li>
                 )
             })}
